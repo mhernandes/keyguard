083db5a9f28e6d37d29c0554080149ccb9451b4b
Matheus Hernandes <midia.matheus@gmail.com>
2016-12-06 10:05:50 -0200

    add menu structure

--- u/index.php
+++ u/index.php
@@ -1,4 +1,10 @@
 <?php
-	$page_name = 'Login';
+	$page_name = 'KeyGuard - PassManager';
 	include 'partials/header.php';
 ?>
+
+<main class="keyguard">
+	<?php include 'partials/menu.php'; ?>
+</main>
+
+<?php include 'partials/footer.php'; ?>
