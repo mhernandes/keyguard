ca0896b22d91ebc4436f42371e8aedd51516d91e
Matheus Hernandes <midia.matheus@gmail.com>
2016-12-16 10:35:15 -0200

    add menu structure

---
+++ u/partials/menu.php
@@ -0,0 +1,12 @@
+<nav class="menu">
+	<a href="" class="menu__logo">
+		<img src="" alt="" class="menu__logo_img">
+	</a>
+
+	<ul class="menu__links">
+		<li class="menu__links_unit"><a href="" icon="">Link</a></li>
+		<li class="menu__links_unit"><a href="" icon="">Link</a></li>
+		<li class="menu__links_unit"><a href="" icon="">Link</a></li>
+		<li class="menu__links_unit"><a href="" icon="">Link</a></li>
+	</ul>
+</nav>
