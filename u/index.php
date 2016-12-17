b0eb6f5ec959c19e57a00990e205c1c9913e5ad5
Matheus Hernandes <midia.matheus@gmail.com>
2016-12-17 01:32:19 -0200

    add identity structure

--- u/index.php
+++ u/index.php
@@ -4,6 +4,20 @@
 ?>
 
 <?php include 'partials/menu.php'; ?>
+
+<section class="identity">
+	<h2 class="identity__title">Title</h2>
+
+	<ul class="identity__buttons">
+		<li class="identity__buttons_unit">
+			<a href="">A</a>
+			<a href="">A</a>
+			<a href="">A</a>
+			<a href="">A</a>
+		</li>
+	</ul>
+</section>
+
 <section class="see">
 	<?php for ($i = 0; $i < 4; $i++) { ?>
 		<article class="see__unit" color="">
