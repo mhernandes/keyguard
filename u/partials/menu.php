32250b2b57c2ba56ca93f2260bd8f9bbcf203722
Matheus Hernandes <midia.matheus@gmail.com>
2016-12-17 00:36:43 -0200

    fix menu structure

--- u/partials/menu.php
+++ u/partials/menu.php
@@ -1,12 +1,36 @@
 <nav class="menu">
-	<a href="" class="menu__logo">
-		<img src="" alt="" class="menu__logo_img">
+	<a href="" class="menu__username">
+		<i class="menu__username_icon" icon="">i</i>
+		User Name
 	</a>
 
 	<ul class="menu__links">
-		<li class="menu__links_unit"><a href="" icon="">Link</a></li>
-		<li class="menu__links_unit"><a href="" icon="">Link</a></li>
-		<li class="menu__links_unit"><a href="" icon="">Link</a></li>
-		<li class="menu__links_unit"><a href="" icon="">Link</a></li>
+		<li class="menu__links_unit">
+			<a href="">
+				<i class="menu__links_unit_icon" icon=""></i>
+				Link
+			</a>
+		</li>
+
+		<li class="menu__links_unit">
+			<a href="">
+				<i class="menu__links_unit_icon" icon=""></i>
+				Link
+			</a>
+		</li>
+
+		<li class="menu__links_unit">
+			<a href="">
+				<i class="menu__links_unit_icon" icon=""></i>
+				Link
+			</a>
+		</li>
+
+		<li class="menu__links_unit">
+			<a href="">
+				<i class="menu__links_unit_icon" icon=""></i>
+				Link
+			</a>
+		</li>
 	</ul>
 </nav>
