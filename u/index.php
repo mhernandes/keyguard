b0da331394523e633d9b975b06d4dc86fecb6b42
Matheus Hernandes <midia.matheus@gmail.com>
2016-12-17 02:24:27 -0200

    clean u/index

--- u/index.php
+++ u/index.php
@@ -7,12 +7,4 @@
 
 <?php include 'partials/identity.php'; ?>
 
-<section class="see">
-	<?php for ($i = 0; $i < 4; $i++) { ?>
-		<article class="see__unit" color="">
-			<a href="" class="see__unit_link">Facebook</a>
-		</article>
-	<?php } ?>
-</section>
-
 <?php include 'partials/footer.php'; ?>
---
+++ u/view/see.php
@@ -0,0 +1,7 @@
+<section class="see">
+	<?php for ($i = 0; $i < 4; $i++) { ?>
+		<article class="see__unit" color="">
+			<a href="" class="see__unit_link">Facebook</a>
+		</article>
+	<?php } ?>
+</section>
