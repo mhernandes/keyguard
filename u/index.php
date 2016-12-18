01fb0d9cd1d729156511c67f2c3eddb2a84f6494
Matheus Hernandes <midia.matheus@gmail.com>
2016-12-18 19:17:03 -0200

    fix create

--- u/index.php
+++ u/index.php
@@ -10,7 +10,16 @@
 <section class="create">
 	<form action="" class="create__form">
 		<label for="" class="create__form_label">Name</label>
-		<input type="text" class="create__form_input" name="" placeholder="" id="">
+		<input type="text" class="create__form_input" name="" placeholder="Facebook" id="">
+
+		<label for="" class="create__form_label">Email</label>
+		<input type="text" class="create__form_input" name="" placeholder="your@email.com" id="">
+		
+		<label for="" class="create__form_label">Username</label>
+		<input type="text" class="create__form_input" name="" placeholder="@someone" id="">
+		
+		<label for="" class="create__form_label">Password</label>
+		<input type="password" class="create__form_input" name="" id="">
 	</form>
 </section>
 
