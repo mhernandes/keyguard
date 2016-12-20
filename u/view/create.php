c6636e7addb58553480ff3486be2018bde6a89a7
Matheus Hernandes <midia.matheus@gmail.com>
2016-12-20 00:38:51 -0200

    add standar button to create form

---
+++ u/view/create.php
@@ -0,0 +1,17 @@
+<section class="create">
+	<form action="" class="create__form">
+		<label for="" class="create__form_label">Name</label>
+		<input type="text" class="create__form_input" name="" placeholder="Facebook" id="">
+
+		<label for="" class="create__form_label">Email</label>
+		<input type="text" class="create__form_input" name="" placeholder="your@email.com" id="">
+		
+		<label for="" class="create__form_label">Username</label>
+		<input type="text" class="create__form_input" name="" placeholder="@someone" id="">
+		
+		<label for="" class="create__form_label">Password</label>
+		<input type="password" class="create__form_input" name="" id="">
+
+		<input type="submit" class="create__form_input" value="Create">
+	</form>
+</section>
