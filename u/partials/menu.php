672235a8a7bce9395d4f12e735a321fb5767cf07
Matheus Hernandes <midia.matheus@gmail.com>
2016-12-29 17:33:31 -0200

    fix menu links

--- u/partials/menu.php
+++ u/partials/menu.php
@@ -1,35 +1,34 @@
 <nav class="menu">
 	<a href="" class="menu__username">
-		<i class="menu__username_icon" icon="">i</i>
 		User Name
 	</a>
 
 	<ul class="menu__links">
 		<li class="menu__links_unit">
-			<a href="">
+			<a href="/keyguard/u/">
 				<i class="menu__links_unit_icon" icon=""></i>
-				Link
+				Home
 			</a>
 		</li>
 
 		<li class="menu__links_unit">
-			<a href="">
+			<a href="create">
 				<i class="menu__links_unit_icon" icon=""></i>
-				Link
+				Create
 			</a>
 		</li>
 
 		<li class="menu__links_unit">
-			<a href="">
+			<a href="edit">
 				<i class="menu__links_unit_icon" icon=""></i>
-				Link
+				Edit
 			</a>
 		</li>
 
 		<li class="menu__links_unit">
-			<a href="">
+			<a href="generate">
 				<i class="menu__links_unit_icon" icon=""></i>
-				Link
+				Generate
 			</a>
 		</li>
 	</ul>
