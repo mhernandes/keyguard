9d7329bbbee1cbd34da3e16f20076ea3086417b2
Matheus Hernandes <midia.matheus@gmail.com>
2017-01-05 16:34:37 -0200

    add insert test values to db

--- u/dao/bank.sql
+++ u/dao/bank.sql
@@ -18,3 +18,15 @@ CREATE TABLE accounts (
 	register_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	CONSTRAINT accounts_fk FOREIGN KEY (mk_user)  REFERENCES users(mk) ON DELETE RESTRICT ON UPDATE RESTRICT
 )ENGINE = InnoDB;
+
+INSERT INTO users(name, username, email, password) 
+VALUES('Matheus', 'matheus', 'midia.matheus@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef');
+
+INSERT INTO accounts(mk_user, title, slug, email, password) 
+VALUES(1, 'Facebook', 'facebook', 'midia.matheus@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef');
+
+INSERT INTO accounts(mk_user, title, slug, email, password) 
+VALUES(1, 'Snapchat', 'snapchat', 'midia.matheus@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef');
+
+INSERT INTO accounts(mk_user, title, slug, email, password) 
+VALUES(1, 'Instagram', 'instagram', 'midia.matheus@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef');
