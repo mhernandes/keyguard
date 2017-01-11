eacd70dd8e2a170b008c7f8597f01fd19848539f
Matheus Hernandes <midia.matheus@gmail.com>
2017-01-10 23:42:39 -0200

    change manage login class

--- u/login/m.login.class.php
+++ u/login/m.login.class.php
@@ -8,7 +8,19 @@
 	    public $login;
 
 	    public function __construct() {
-	        return $this->login = Login::getInstance();
+	        $this->login = Login::getInstance();
+	    }
+
+	    public function set($userdata = array()) {
+	    	$this->login->setData();
+	    }
+
+	    public function get() {
+	    	$this->login->getData();
+	    }
+
+	    public function check() {
+	    	$this->login->checkUser();
 	    }
 	}
 ?>
