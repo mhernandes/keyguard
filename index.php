a1d5afdeece07fd2957069b079adf66d79030364
Matheus Hernandes <midia.matheus@gmail.com>
2017-01-12 23:24:01 -0200

    remove composer

--- index.php
+++ index.php
@@ -1,19 +1,4 @@
 <?php
-	$page_name = 'Login';
-	include('u/partials/header.php');
+	require '/vendor/autoload.php';
+	$ac = new DAO\Access();
 ?>
-
-<section class="login">
-	<form action="" class="login__form" method="post">
-		<label class="login__form_label" for="email">Email</label>
-		<input type="text" class="login__form_input" id="email" name="email" placeholder="Email">
-		
-		<label class="login__form_label" for="pass">Password</label>
-		<input type="text" class="login__form_input" id="pass" name="password" placeholder="Password">
-		
-		<input type="submit" class="login__form_submit" value="Log in" name="submit">
-		<a href="" class="login__form_forgot">Forgot your password?</a>
-	</form>
-</section>
-
-<?php include('u/partials/footer.php'); ?>
--- u/tester.php
+++
@@ -1,6 +0,0 @@
-<?php
-	require '/vendor/autoload.php';
-	//require 'DAO/access.class.php';
-	//use DAO\Access;
-	$ac = new DAO\Access();
-?>
