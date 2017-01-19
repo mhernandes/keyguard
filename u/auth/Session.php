cbcbd1dd81b7264dcd49e8d61f5702b091964892
Matheus Hernandes <midia.matheus@gmail.com>
2017-01-19 01:00:04 -0200

    add basic methos to session class

--- u/auth/Session.php
+++ u/auth/Session.php
@@ -1,15 +1,50 @@
 <?php
 	namespace Auth;
-	
+	use DAO\ManageAccess;
+	use \PDO;
 	/**
 	 * summary
 	 */
 	class Session {
+		private $access;
+		private $session_data;
 	    /**
 	     * summary
 	     */
 	    public function __construct() {
-	        
+	        $this->access = new ManageAccess();
+	    }
+
+	    public function startSession($session_name = "KeyGuard") {
+	    	if (session_status() !== PHP_SESSION_ACTIVE) { return false; }
+	    	
+	    	session_name($session_name);
+	    	session_start();
+	    	return $this;
+	    }
+
+	    public function setSessionData($data = array()) {
+	    	foreach ($data as $key => $value) {
+	    		$this->session_data[$key] = $value;
+	    	}
+
+	    	return $this;
+	    }
+
+	    public function registerSessionData() {
+	    	$data_to_register = $this->session_data;
+	    	
+	    	foreach ($data_to_register as $key => $value) {
+	    		$_SESSION[$key] = $value;
+	    	}
+
+	    	return $this;
+	    }
+
+	    public function destroy() {
+	    	session_destroy();
+
+	    	return $this;
 	    }
 	}
 ?>
